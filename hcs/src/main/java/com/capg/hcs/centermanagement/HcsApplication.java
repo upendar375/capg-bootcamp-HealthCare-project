bf7a8e6f76bfc7965350bc6b1f5d6179ce51ada0
home <home@DESKTOP-852997S>
2020-11-14 12:57:51 +0530

    First commit By Upendar.S

---
+++ hcs/src/main/java/com/capg/hcs/centermanagement/HcsApplication.java
@@ -0,0 +1,13 @@
+package com.capg.hcs.centermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcsApplication.class, args);
+	}
+
+}
---
+++ hcs/src/test/java/com/capg/hcs/centermanagement/HcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.hcs.centermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
